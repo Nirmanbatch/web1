52820417dccb8cb217ae1738e7149dbac249dccb
Nirmanbatch <111363110+Nirmanbatch@users.noreply.github.com>
2023-01-11 10:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Nirmanbatch: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@github.com/Nirmanbatch/File-Sharing-Bot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
